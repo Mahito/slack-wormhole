3aa28d9b1ae6bd9cab21b4f9fa53c88201d6fc55
Mahito Ogura <earthdragon77@gmail.com>
2020-01-14 16:23:09 +0900

    Fix slack_wormhole

--- lib/slack_wormhole.rb
+++ lib/slack_wormhole.rb
@@ -3,5 +3,11 @@
 require_relative  'publish'
 require_relative  'subscribe'
 
-SlackWormhole::Subscriber.start
+subscribers = []
+subscription_names = ENV['WORMHOLE_SUBSCRIPTION_NAMES']
+
+subscription_names.split(',').each do |name|
+  subscribers << SlackWormhole::Subscriber.start(name)
+end
+
 SlackWormhole::Publish.start
